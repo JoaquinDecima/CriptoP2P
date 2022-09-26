2b5088e258dfa0142a7de9aeb82ef770b293566a
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-09-26 11:22:35 -0300

    Create new UserService

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoe/backenddesappapi/service/UserService.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.desapp.grupoe.backenddesappapi.service
+
+import ar.edu.unq.desapp.grupoe.backenddesappapi.model.User
+import ar.edu.unq.desapp.grupoe.backenddesappapi.repository.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class UserService (private val userRepository: UserRepository){
+    fun getAllUsers(): MutableList<User> = userRepository.findAll()
+    fun saveUser(user: User) = userRepository.save(user)
+}
