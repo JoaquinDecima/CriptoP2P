6dc6772752eaeca16cb322e2e56b42f3fa966200
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-18 20:31:52 -0300

    Add Swagger conf

---
+++ src/config/swaggerConf.js
@@ -0,0 +1,16 @@
+export const swaggerConf = {
+    SwaggerDefinition: {
+        info: {
+            title: 'API Documentation DesApp',
+            version: '1.0.0',
+            description: 'API Documentation Desarrollo de Aplicaciones - UNQ',
+            contact: {
+                name: 'Joaquin (Pato) Decima',
+                email: 'jdecima@vasak.net.ar',
+                url: 'https://joaquindecima.ml/'
+            },
+        }
+    },
+    basePath: '/docs',
+    apis: [],
+};
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 import { environment } from './config/enviroment';
+import { swaggerConf } from './config/swaggerConf';
 
 // Create Express server
 const app = express();
@@ -10,6 +13,9 @@ const port = environment.PORT;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Swagger configuration
+const swaggerDefinition = swaggerJSDoc(swaggerConf);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDefinition));
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
