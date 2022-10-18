94bfd9c288a81988c63c47e4a0e4b9f99aad2293
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-18 19:23:05 -0300

    Refactor proyect

--- src/main/kotlin/ar/edu/unq/desapp/grupoe/backenddesappapi/repository/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.desapp.grupoe.backenddesappapi.repository
-
-import ar.edu.unq.desapp.grupoe.backenddesappapi.model.User
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface UserRepository : MongoRepository<User, String> {
-}
