120c4b438512a63cde682b08be6cfea983d8210c
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-19 20:10:37 -0300

    Remove unused variable

--- src/config/enviroment.js
+++ src/config/enviroment.js
@@ -6,6 +6,5 @@ export const environment = {
     PORT: parseInt(process.env.PORT || 8080),
     MONGODB_HOST: process.env.MONGODB_HOST,
     JWT_SECRET: process.env.JWT_SECRET || 'secret',
-    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
     HASH_SALT: parseInt(process.env.HASH_SALT || '2')
 };
