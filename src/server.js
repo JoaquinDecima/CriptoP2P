c64a62cc3b4ad3f3274111d4d1ec7f62cc32f40f
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-19 21:54:36 -0300

    Fix security error

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGODB_HOST);
 // Express configuration
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.disable("x-powered-by");
 
 // Swagger configuration
 const swaggerDefinition = swaggerJSDoc(swaggerConf);
