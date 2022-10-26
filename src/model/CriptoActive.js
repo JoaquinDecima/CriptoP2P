1d3c285c0d534e162e4071121c88f81a93b2fd42
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-26 19:27:25 -0300

    Remove Name Criptoactivo

--- src/model/CriptoActive.js
+++ src/model/CriptoActive.js
@@ -1,12 +1,6 @@
 import mongoose from 'mongoose';
 
 const CriptoActiveSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        min: 3,
-        max: 30,
-    },
     symbol: {
         type: String,
         required: true,
