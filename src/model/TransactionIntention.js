192d6dc390503e85a72d506655e078e3230afac4
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-26 18:07:18 -0300

    Add Model TransactionIntention

---
+++ src/model/TransactionIntention.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const TransactionIntentionSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    criptoActive: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CriptoActive',
+        required: true,
+    },
+    criptoActiveValue: {
+        type: Number,
+        required: true,
+    },
+    criptoActiveAmount: {
+        type: Number,
+        required: true,
+    },
+    ARSValue: {
+        type: Number,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    type: {
+        type: String,
+        enum: ['compra', 'venta'],
+        required: true,
+    },
+});
+
+export const TransactionIntention = mongoose.model('TransactionIntention', TransactionIntentionSchema);
