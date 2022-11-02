a924ae3b9095a0dc4f46bdd79ed0968c656dc292
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-11-02 14:41:07 -0300

    Remove unuse log

--- src/middleware/isAuth.js
+++ src/middleware/isAuth.js
@@ -1,7 +1,6 @@
 import TokenManager from '../tools/TokenManager.js';
 
 export default function isAuthenticated(req, res, next) {
-    console.info(TokenManager.verifyToken(req.headers.token))
 	try {
 		if(TokenManager.verifyToken(req.headers.token)){
             req.headers.user = TokenManager.verifyToken(req.headers.token).user;
