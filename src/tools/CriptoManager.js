ecbd2916ae7762cb0a47aeb198d64d810e5017a8
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-11-01 21:42:29 -0300

    Transform to ARS value

--- src/tools/CriptoManager.js
+++ src/tools/CriptoManager.js
@@ -2,22 +2,25 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 import CriptoActiveService from '../service/CriptoActiveService.js';
 import { getPrices } from './BinanceAPI.js';
+import { getValue } from './USDAPI.js';
 
 class CriptoManager {
 
-    ingestCryptoData() {
+    async ingestCryptoData() {
+        const USDPrice = await getValue();
         const cryptoData = require('../data/criptoData.json');
         cryptoData.forEach(async (crypto) => {
             const price = await getPrices(crypto);
             const cryptoModel = {
                 symbol: price.symbol,
-                price: price.price,
+                price: price.price * USDPrice.compra,
             }
             CriptoActiveService.createCriptoActive(cryptoModel);
         });
     }
 
-    updateCryptoData() {
+    async updateCryptoData() {
+        const USDPrice = await getValue();
         CriptoActiveService.getAllCriptoActives()
             .then(criptoActive => {
                 criptoActive.forEach(async (crypto) => {
@@ -28,7 +31,7 @@ class CriptoManager {
                     };
                     crypto.historical.push(oldprice)
                     crypto.historical = crypto.historical.filter((item) => ((new Date - item.date)/(1000*60*60*24)) < 1);
-                    crypto.price = price.price;
+                    crypto.price = price.price * USDPrice.compra;
                     crypto.updatetime = Date.now();
                     crypto.save();
                 });
