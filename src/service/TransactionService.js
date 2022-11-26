e317bd7d111900aab10d8cd16644c957e6098020
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-11-26 19:33:14 -0300

    Add concret transaction

--- src/service/TransactionService.js
+++ src/service/TransactionService.js
@@ -1,4 +1,5 @@
 import { TransactionRepository } from "../repositories/TransactionRepisitory.js";
+import TransactionIntentionService from "./TransactionIntentionService.js";
 
 class TransactionService {
     tRepository = new TransactionRepository();
@@ -11,8 +12,33 @@ class TransactionService {
         return this.tRepository.getTransactionById(id);
     }
 
-    createTransaction(transaction) {
-        return this.tRepository.createTransaction(transaction);
+    concretTransaction(idIntention, user) {
+        TransactionIntentionService.getTransactionIntentionById(idIntention)
+            .then(intention => {
+                if (!intention) {
+                    throw new Error("Intention not found");
+                }
+                if (intention.user.id === user.id) {
+                    throw new Error("You cannot accept your own transaction");
+                }
+                const newTransacation = {
+                    criptoActive: intention.criptoActive,
+                    nominalValue: intention.nominalValue,
+                    amount: intention.amount,
+                    reputation: 0,
+                }
+                if (intention.operation === "COMPRA") {
+                    newTransacation.buyer = user;
+                    newTransacation.seller = intention.user;
+                } else {
+                    newTransacation.buyer = intention.user;
+                    newTransacation.seller = user;
+                }
+                return this.tRepository.concretTransaction(newTransacation);
+            })
+            .catch(err => {
+                throw new Error("Error concret transaction: " + err.message);
+            });
     }
 
     deleteTransaction(id) {
