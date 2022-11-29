4f0583bad914f7c666b07b1455556babe50dab54
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-11-29 17:10:25 -0300

    Fix user error fix type function

--- src/service/TransactionService.js
+++ src/service/TransactionService.js
@@ -1,5 +1,6 @@
 import { TransactionRepository } from "../repositories/TransactionRepisitory.js";
 import ChangeState from "../tools/ChangeState.js";
+import { validatePorcent } from "../tools/Validators.js";
 import TransactionIntentionService from "./TransactionIntentionService.js";
 
 class TransactionService {
@@ -18,33 +19,30 @@ class TransactionService {
         return this.tRepository.getTransactionById(id);
     }
 
-    concretTransaction(idIntention, user) {
-        TransactionIntentionService.getTransactionIntentionById(idIntention)
-            .then(intention => {
-                if (!intention) {
-                    throw new Error("Intention not found");
-                }
-                if (intention.user.id === user.id) {
-                    throw new Error("You cannot accept your own transaction");
-                }
-                const newTransacation = {
-                    criptoActive: intention.criptoActive,
-                    nominalValue: intention.nominalValue,
-                    amount: intention.amount,
-                    reputation: 0,
-                }
-                if (intention.operation === "COMPRA") {
-                    newTransacation.buyer = user;
-                    newTransacation.seller = intention.user;
-                } else {
-                    newTransacation.buyer = intention.user;
-                    newTransacation.seller = user;
-                }
-                return this.tRepository.concretTransaction(newTransacation);
-            })
-            .catch(err => {
-                throw new Error("Error concret transaction: " + err.message);
-            });
+    async concretTransaction(idIntention, user) {
+        const intention = await TransactionIntentionService.getTransactionIntentionById(idIntention);
+
+        if (!intention) {
+            throw new Error("Intention not found");
+        }
+        if (intention.user.email == user.email) {
+            throw new Error("You cannot accept your own transaction");
+        }
+        validatePorcent(intention.nominalValue, intention.criptoActive.price);
+        const newTransacation = {
+            criptoActive: intention.criptoActive,
+            nominalValue: intention.nominalValue,
+            amount: intention.amount,
+            reputation: 0,
+        }
+        if (intention.operation === "COMPRA") {
+            newTransacation.buyer = user;
+            newTransacation.seller = intention.user;
+        } else {
+            newTransacation.buyer = intention.user;
+            newTransacation.seller = user;
+        }
+        return this.tRepository.createTransaction(newTransacation);
     }
 
     async advanceState(user, transactionID) {
