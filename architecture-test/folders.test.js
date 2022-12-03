6b12dc6e101c4f6d4a7b8f72f421053e5196fc32
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-12-03 19:46:06 -0300

    Add Base acht test

---
+++ architecture-test/folders.test.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+const Archijs = require("archijs");
+
+describe("Verify Name Files and Folders", () => {
+    it("/repositories/*Repository", () => {
+      const project = Archijs.parseFromPath("src");
+      
+      const rule = Archijs
+        .defineThat()
+        .folder('repositories')
+        .withNameMatching('Repository')
+        .should()
+        .matchChildrensName('Repository')
+  
+      expect(project).toMatchArch(rule);
+    });
+    it("/service/*Service", () => {
+      const project = Archijs.parseFromPath("src");
+      
+      const rule = Archijs
+        .defineThat()
+        .folder('service')
+        .withNameMatching('Service')
+        .should()
+        .matchChildrensName('Service')
+  
+      expect(project).toMatchArch(rule);
+    });
+    it("/router/*Router", () => {
+      const project = Archijs.parseFromPath("src");
+      
+      const rule = Archijs
+        .defineThat()
+        .folder('router')
+        .withNameMatching('Router')
+        .should()
+        .matchChildrensName('Router')
+  
+      expect(project).toMatchArch(rule);
+    });
+  });
