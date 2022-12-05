b8022ac414fc8254a99bc80d36a925380c091562
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-12-05 07:55:12 -0300

    Use not import

--- architecture-test/import.test.js
+++ architecture-test/import.test.js
@@ -1,24 +1,24 @@
 /* eslint-disable no-undef */
-const checkImportsContains = require('./vasak-archt-js/tools/checkImport.cjs');
+const {checkImportsContains, checkImportsNotContains} = require('./vasak-archt-js/tools/checkImport.cjs');
 
 describe("Verify imports", () => {
     it("Verify imports in model", () => {
         expect(checkImportsContains('./src/model', 'mongoose')).toBe(true);
-        expect(checkImportsContains('./src/model', 'express')).toBe(false);
+        expect(checkImportsNotContains('./src/model', 'express')).toBe(true);
     });
     it("Verify imports in repositories", () => {
         expect(checkImportsContains('./src/repositories', 'mongoose')).toBe(true);
-        expect(checkImportsContains('./src/repositories', 'express')).toBe(false);
+        expect(checkImportsNotContains('./src/repositories', 'express')).toBe(true);
     });
     it("Verify imports in service", () => {
         expect(checkImportsContains('./src/service', 'Repository')).toBe(true);
-        expect(checkImportsContains('./src/service', 'mongoose')).toBe(false);
-        expect(checkImportsContains('./src/service', 'express')).toBe(false);
+        expect(checkImportsNotContains('./src/service', 'mongoose')).toBe(true);
+        expect(checkImportsNotContains('./src/service', 'express')).toBe(true);
     });
     it("Verify imports in router", () => {
         expect(checkImportsContains('./src/router', 'express')).toBe(true);
         expect(checkImportsContains('./src/router', 'Service')).toBe(true);
-        expect(checkImportsContains('./src/router', 'Repository')).toBe(false);
-        expect(checkImportsContains('./src/router', 'mongoose')).toBe(false);
+        expect(checkImportsNotContains('./src/router', 'Repository')).toBe(true);
+        expect(checkImportsNotContains('./src/router', 'mongoose')).toBe(true);
     });
 });
