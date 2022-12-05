e0eeedeb74ddb01b5149acba1560e06a93159c14
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-12-05 07:55:05 -0300

    Fix import check Add not import

--- architecture-test/vasak-archt-js/tools/checkImport.cjs
+++ architecture-test/vasak-archt-js/tools/checkImport.cjs
@@ -1,21 +1,37 @@
 const getAllImports = require('./filesMamnager.cjs');
 
 function checkImportsContains(dir, contains) {
+    let containsBool = true;
+    let imports = getAllImports(dir);
+    imports = imports.map(file => file.map(line => line.includes(contains)));
+    
+    for (let file of imports) {
+        let fileBool = false;
+        for (let imp of file) {
+            fileBool = fileBool || imp;
+        }
+        containsBool = containsBool && fileBool;
+    }
+
+    return containsBool;
+}
+
+function checkImportsNotContains(dir, contains) {
     let containsBool = false;
     let imports = getAllImports(dir);
     imports = imports.map(file => file.map(line => line.includes(contains)));
 
     for (let file of imports) {
         let fileBool = false;
-        for (let imp of file){
+        for (let imp of file) {
             fileBool = fileBool || imp;
         }
         containsBool = containsBool || fileBool;
     }
 
-    return containsBool;
+    return !containsBool;
 }
 
-module.exports = checkImportsContains;
+module.exports = { checkImportsContains, checkImportsNotContains };
 
-//console.log(checkImportsContains('./src/model', 'mongoose'));
+//console.log(checkImportsContains('./src/router', 'express'));
