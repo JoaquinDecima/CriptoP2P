fa71a36a1d20c4a1dd67585f1335f05df04b11a8
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-12-05 13:16:32 -0300

    Add strict check in test

---
+++ architecture-test/strict.test.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+const { checkFileNotUseStrict, checkFolderNotUseStrict } = require('./vasak-archt-js/checkStrict.cjs');
+
+describe("Verify strict", () => {
+    it("Verify strict in model", () => {
+        expect(checkFolderNotUseStrict('./src/model')).toBe(true);
+    });
+    it("Verify strict in repositories", () => {
+        expect(checkFolderNotUseStrict('./src/repositories')).toBe(true);
+    });
+    it("Verify strict in service", () => {
+        expect(checkFolderNotUseStrict('./src/service')).toBe(true);
+    });
+    it("Verify strict in router", () => {
+        expect(checkFolderNotUseStrict('./src/router')).toBe(true);
+    });
+    it("Verify strict in middleware", () => {
+        expect(checkFolderNotUseStrict('./src/middleware')).toBe(true);
+    });
+    it("Verify strict in data", () => {
+        expect(checkFolderNotUseStrict('./src/data')).toBe(true);
+    });
+    it("Verify strict in config", () => {
+        expect(checkFolderNotUseStrict('./src/config')).toBe(true);
+    });
+    it("Verify strict in tools", () => {
+        expect(checkFolderNotUseStrict('./src/tools')).toBe(true);
+    });
+    it("Verify strict in app.js", () => {
+        expect(checkFileNotUseStrict('./src/server.js')).toBe(true);
+    })
+});
